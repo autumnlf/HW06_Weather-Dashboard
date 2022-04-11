7ee5ee0984bf82e98bd0796ba5124ed2245ece27
Autumn Fields <autumnF12@gmail.com>
2022-04-11 19:41:37 -0400

    updated js to change weather images with current and forecasted weather conditions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var key = '27b5e63c65752cfa7f1cc398fef6d406';
+var key = '27b5e63c65752cfa7f1cc398fef6d406'; //API key
 var currentDate = moment().format("dddd MMMM Do, YYYY");
 var currentTime = moment().format("hh:mm a");
 
@@ -13,17 +13,20 @@ var day3 = document.getElementById("day3");
 var day4 = document.getElementById("day4");
 var day5 = document.getElementById("day5");
 
+//displaying the current date and time
 date.innerText = currentDate;
 time.innerText = currentTime;
-day1.innerText = moment().add(1, 'day').format('MM/DD');
-day2.innerText = moment().add(2, 'day').format('MM/DD');
-day3.innerText = moment().add(3, 'day').format('MM/DD');
-day4.innerText = moment().add(4, 'day').format('MM/DD');
-day5.innerText = moment().add(5, 'day').format('MM/DD');
+// day1.innerText = moment().add(1, 'day').format('MM/DD');
+// day2.innerText = moment().add(2, 'day').format('MM/DD');
+// day3.innerText = moment().add(3, 'day').format('MM/DD');
+// day4.innerText = moment().add(4, 'day').format('MM/DD');
+// day5.innerText = moment().add(5, 'day').format('MM/DD');
 
+//this array is pre-filled in cities that will be replaced with user search history.
 var cities = ['Knoxville', 'Pigeon Forge', 'Atlanta', 'Milledgeville', 'Dublin'];
 console.log(cities);
 
+//setting up variables
 var searchbtn = document.getElementById("searchbtn");
 var searchbar = document.getElementById("searchbar");
 var city1 = document.getElementById("city1");
@@ -40,12 +43,20 @@ var currentuv = document.getElementById("currentuv");
 var hltemp = document.getElementById("hltemp");
 var hlwind = document.getElementById("hlwind");
 var hluv = document.getElementById("hluv");
+var currentweather = document.getElementById("weather");
+var currentImg = document.getElementById("currentImg");
 
+var forecastContainer = document.getElementById("forecastContainer");
 var temp1 = document.getElementById("temp1");
 var temp2 = document.getElementById("temp2");
 var temp3 = document.getElementById("temp3");
 var temp4 = document.getElementById("temp4");
 var temp5 = document.getElementById("temp5");
+var forecastImg0 = document.getElementById("forecast0");
+var forecastImg1 = document.getElementById("forecast1");
+var forecastImg2 = document.getElementById("forecast2");
+var forecastImg3 = document.getElementById("forecast3");
+var forecastImg4 = document.getElementById("forecast4");
 
 
 searchbtn.onclick = citysearch;
@@ -70,7 +81,7 @@ function citysearch(){
     weathersearch();
 }
 
-
+//latitude and longitude of the city called, will be used in another api data fetch
 var lat = '';
 var lon = '';
 
@@ -78,6 +89,7 @@ function weathersearch() {
   var weatherapi = 'https://api.openweathermap.org/data/2.5/weather?q=' + cities[0] + '&units=imperial&appid=' + key;
   citytitle.innerText = cities[0];
 
+  // making a fetch from open call api to get city weather data
   fetch(weatherapi)
   .then(function (response) {
     return response.json();
@@ -85,16 +97,20 @@ function weathersearch() {
   .then(function (data) {
     console.log(data)
 
+    //display current temp and high/low for day
     console.log(data.main.temp);
     currenttemp.innerText = data.main.temp + '°';
-    hltemp.innerText = 'H/L: ' + data.main.temp_max + '°/' + data.main.temp_min + '°';
+    hltemp.innerText = 'H/L:  ' + Math.round(data.main.temp_max) + '° / ' + Math.round(data.main.temp_min) + '°';
 
+    //display current humidity
     currenthumidity.innerText = data.main.humidity;
 
+    //display current wind and degree it is blowing
     console.log(data.wind);
     currentwind.innerText = data.wind.speed + ' mph';
     hlwind.innerText = data.wind.deg + ' deg';
 
+    //get latitude and longitude of city to use for another fetch from open call
     lat = data.coord.lat;
     lon = data.coord.lon;
     console.log(lat);
@@ -102,8 +118,6 @@ function weathersearch() {
 
     forecast();
   });
-
-  
     
 }
 
@@ -111,6 +125,10 @@ function weathersearch() {
 function forecast(){
   var forecastapi = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + key;
   
+  //to clear the current 5-day forecast
+  forecastContainer.replaceChildren();
+
+  //this fetch from open call api gathers a few more details than the city call and gives the future forecast
   fetch(forecastapi)
   .then(function (response) {
     return response.json();
@@ -118,9 +136,11 @@ function forecast(){
   .then(function (data) {
     console.log(data)
 
+    //display UV
     console.log(data.current.uvi);
     currentuv.innerText = data.current.uvi;
 
+    //make range for UV index low to extreme and give a new class to change text color based on range.
     if (data.current.uvi < 2.999){
       hluv.innerText = 'Low';
       hluv.setAttribute('class', 'low');
@@ -147,13 +167,96 @@ function forecast(){
       currentuv.setAttribute('class', 'extreme');
     }
 
+    //set temperature display for 5-day forecasts
+    // temp1.innerText = Math.round(data.daily[0].temp.day) + '°';
+    // temp2.innerText = Math.round(data.daily[1].temp.day) + '°';
+    // temp3.innerText = Math.round(data.daily[2].temp.day) + '°';
+    // temp4.innerText = Math.round(data.daily[3].temp.day) + '°';
+    // temp5.innerText = Math.round(data.daily[4].temp.day) + '°';
 
-    temp1.innerText = Math.round(data.daily[0].temp.day) + '°';
-    temp2.innerText = Math.round(data.daily[1].temp.day) + '°';
-    temp3.innerText = Math.round(data.daily[2].temp.day) + '°';
-    temp4.innerText = Math.round(data.daily[3].temp.day) + '°';
-    temp5.innerText = Math.round(data.daily[4].temp.day) + '°';
+    //displaying the curennt weather description
+    currentweather.innerText = data.current.weather[0].description;
 
+    //setting the current weather image
+    if (data.current.weather[0].id < 300){
+      currentImg.setAttribute("style", "background-image: url('./assets/thunderstorm.jpg');");
+    }
+    else if (data.current.weather[0].id < 500){
+      currentImg.setAttribute("style", "background-image: url('./assets/drizzle.jpg');");
+    }
+    else if (data.current.weather[0].id < 502){
+      currentImg.setAttribute("style", "background-image: url('./assets/light rain.jpg');");
+    }
+    else if (data.current.weather[0].id < 600){
+      currentImg.setAttribute("style", "background-image: url('./assets/heavy rain.jpg');");
+    }
+    else if (data.current.weather[0].id < 700){
+      currentImg.setAttribute("style", "background-image: url('./assets/snow.jpg');");
+    }
+    else if (data.current.weather[0].id < 800){
+      currentImg.setAttribute("style", "background-image: url('./assets/fog.jpg');");
+    }
+    else if (data.current.weather[0].id == 800){
+      currentImg.setAttribute("style", "background-image: url('./assets/clear.jpg');");
+    }
+    else if (data.current.weather[0].id < 803){
+      currentImg.setAttribute("style", "background-image: url('./assets/light clouds.jpg');");
+    }
+    else {
+      currentImg.setAttribute("style", "background-image: url('./assets/heavy clouds.jpg');");
+    }
+
+    //creating 5 day forecast
+    for (let i=0; i < 5; i++){
+      //creating the containers for each day
+      var forecastdiv = document.createElement('div');
+      forecastdiv.setAttribute('class', "forecast");
+
+      //creating element for forecast temperature
+      var forecastTemp = document.createElement('p');
+      forecastTemp.innerText = Math.round(data.daily[i].temp.day) + '°';
+
+      //creating element for forecast date
+      var forecastDay = document.createElement('p');
+      forecastDay.innerText = moment().add(i+1, 'day').format('MM/DD');
+
+      // setting background image to match the forecasted weather
+      var x = data.daily[i].weather[0].id;
+      if (x < 300){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/thunderstorm.jpg');");
+      }
+      else if (x < 500){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/drizzle.jpg');");
+      }
+      else if (x < 502){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/light rain.jpg');");
+      }
+      else if (x < 600){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/heavy rain.jpg');");
+      }
+      else if (x < 700){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/snow.jpg');");
+      }
+      else if (x < 800){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/fog.jpg');");
+      }
+      else if (x == 800){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/clear.jpg');");
+      }
+      else if (x < 803){
+        forecastdiv.setAttribute("style", "background-image: url('./assets/light clouds.jpg');");
+      }
+      else {
+        forecastdiv.setAttribute("style", "background-image: url('./assets/heavy clouds.jpg');");
+      }
+
+      //appending the containers to the correct section of the page
+      forecastContainer.appendChild(forecastdiv);
+      forecastdiv.appendChild(forecastTemp);
+      forecastdiv.appendChild(forecastDay);
+
+    }
+   
 
   });
 
@@ -181,7 +284,7 @@ function weathercity1() {
 
       console.log(data.main.temp);
       currenttemp.innerText = data.main.temp + '°';
-      hltemp.innerText = 'H/L: ' + data.main.temp_max + '°/' + data.main.temp_min + '°';
+      hltemp.innerText = 'H/L:  ' + Math.round(data.main.temp_max) + '° / ' + Math.round(data.main.temp_min) + '°';
 
       currenthumidity.innerText = data.main.humidity;
 
@@ -212,7 +315,7 @@ function weathercity2() {
 
       console.log(data.main.temp);
       currenttemp.innerText = data.main.temp + '°';
-      hltemp.innerText = 'H/L: ' + data.main.temp_max + '°/' + data.main.temp_min + '°';
+      hltemp.innerText = 'H/L:  ' + Math.round(data.main.temp_max) + '° / ' + Math.round(data.main.temp_min) + '°';
 
       currenthumidity.innerText = data.main.humidity;
 
@@ -241,7 +344,7 @@ function weathercity3() {
 
       console.log(data.main.temp);
       currenttemp.innerText = data.main.temp + '°';
-      hltemp.innerText = 'H/L: ' + data.main.temp_max + '°/' + data.main.temp_min + '°';
+      hltemp.innerText = 'H/L:  ' + Math.round(data.main.temp_max) + '° / ' + Math.round(data.main.temp_min) + '°';
 
       currenthumidity.innerText = data.main.humidity;
 
@@ -270,7 +373,7 @@ function weathercity4() {
 
       console.log(data.main.temp);
       currenttemp.innerText = data.main.temp + '°';
-      hltemp.innerText = 'H/L: ' + data.main.temp_max + '°/' + data.main.temp_min + '°';
+      hltemp.innerText = 'H/L:  ' + Math.round(data.main.temp_max) + '° / ' + Math.round(data.main.temp_min) + '°';
 
       currenthumidity.innerText = data.main.humidity;
 
@@ -299,7 +402,7 @@ function weathercity5() {
 
       console.log(data.main.temp);
       currenttemp.innerText = data.main.temp + '°';
-      hltemp.innerText = 'H/L: ' + data.main.temp_max + '°/' + data.main.temp_min + '°';
+      hltemp.innerText = 'H/L:  ' + Math.round(data.main.temp_max) + '° / ' + Math.round(data.main.temp_min) + '°';
 
       currenthumidity.innerText = data.main.humidity;
 
